b0460417b4cfc23b6402b745591c3bec06f6b681
Blagoja Stamatovski <bazestamatovski@gmail.com>
2020-09-25 19:25:29 +0200

    Enable JPA Repositories.

--- src/main/java/codingchallenge/TicTacToe.java
+++ src/main/java/codingchallenge/TicTacToe.java
@@ -3,8 +3,10 @@ package codingchallenge;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class) // TODO: enable security
+@EnableJpaRepositories
 public class TicTacToe {
     public static void main(String[] args) {
         SpringApplication.run(TicTacToe.class);
