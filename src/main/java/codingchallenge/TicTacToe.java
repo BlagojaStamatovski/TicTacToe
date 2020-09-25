c679d8a33670d64bc794a7fba7cc25f13d6adf9e
Blagoja Stamatovski <bazestamatovski@gmail.com>
2020-09-25 19:14:42 +0200

    Disable Spring Security autoconfiguration.

--- src/main/java/codingchallenge/TicTacToe.java
+++ src/main/java/codingchallenge/TicTacToe.java
@@ -1,2 +1,12 @@
-package codingchallenge;public class TicTacToe {
+package codingchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class) // TODO: enable security
+public class TicTacToe {
+    public static void main(String[] args) {
+        SpringApplication.run(TicTacToe.class);
+    }
 }
