0bd657cabbf9a72fc95e140353fb56d6eec0b0b3
Blagoja Stamatovski <bazestamatovski@gmail.com>
2020-09-26 12:09:22 +0200

    Refactor names.

--- src/main/java/com/netcetera/codingchallenge/TicTacToe.java
+++ src/main/java/com/netcetera/codingchallenge/TicTacToe.java
@@ -1,4 +1,4 @@
-package codingchallenge;
+package com.netcetera.codingchallenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
