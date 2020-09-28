b3eb46b132eac51ddbc5ea7f8160a1c1147ce42e
Blagoja Stamatovski <bazestamatovski@gmail.com>
2020-09-28 18:24:27 +0200

    Refactor Controller logic.

--- src/main/java/com/netcetera/codingchallenge/game/tictactoe/rest/GameController.java
+++ src/main/java/com/netcetera/codingchallenge/game/tictactoe/rest/GameController.java
@@ -1,6 +1,7 @@
 package com.netcetera.codingchallenge.game.tictactoe.rest;
 
 import com.netcetera.codingchallenge.game.tictactoe.GameService;
+import com.netcetera.codingchallenge.game.tictactoe.MoveOutcome;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -12,20 +13,29 @@ import java.security.Principal;
 @RequestMapping("/api/v1/tictactoe")
 @PreAuthorize("isAuthenticated()")
 @RequiredArgsConstructor
-public class GameRestController {
+public class GameController {
 
     private final GameService gameService;
 
     @PostMapping("/start")
     @PreAuthorize("hasAuthority('START_GAME')")
     public ResponseEntity<String> startGame() {
-        this.gameService.startGame();
-        return ResponseEntity.ok(this.gameService.getGameDisplayState());
+        final MoveOutcome moveOutcome = this.gameService.startGame();
+        return this.handleMoveOutcome(moveOutcome);
     }
 
     @PutMapping("/move")
     public ResponseEntity<String> makeMove(@RequestBody final MoveRequest moveRequest, final Principal principal) {
-        this.gameService.makeMove(moveRequest.getX(), moveRequest.getY(), principal);
-        return ResponseEntity.ok(this.gameService.getGameDisplayState());
+        final MoveOutcome moveOutcome = this.gameService.makeMove(moveRequest.getX(), moveRequest.getY(), principal);
+        return this.handleMoveOutcome(moveOutcome);
+    }
+
+    private ResponseEntity<String> handleMoveOutcome(final MoveOutcome moveOutcome) {
+        final String toReturn = this.gameService.getGameDisplayState() + "\n" + moveOutcome.getDescription();
+
+        if (!moveOutcome.isValid()) {
+            return ResponseEntity.badRequest().body(toReturn);
+        }
+        return ResponseEntity.ok(toReturn);
     }
 }
