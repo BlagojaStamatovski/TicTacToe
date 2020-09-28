0cd8a9a280c21ec1f0474f7e7f99ab3bf820a333
Blagoja Stamatovski <bazestamatovski@gmail.com>
2020-09-28 23:52:45 +0200

    Change JWT Response DTO.

--- src/main/java/com/netcetera/codingchallenge/authentication/jwt/rest/JWTResponse.java
+++ src/main/java/com/netcetera/codingchallenge/authentication/jwt/rest/JWTResponse.java
@@ -1,5 +1,6 @@
 package com.netcetera.codingchallenge.authentication.jwt.rest;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,5 +10,10 @@ import java.io.Serializable;
 @AllArgsConstructor
 public class JWTResponse implements Serializable {
     private static final long serialVersionUID = 5895787271951299252L;
+
+    @JsonProperty("access_token")
     private String JWToken;
+
+    @JsonProperty("expires_in")
+    private final int expiresIn = 60 * 60;
 }
